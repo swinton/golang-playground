9aa491d03cf1a5703ed02d9869d63ebe703bdba2
Steve Winton <stevewinton@gmail.com>
2020-02-15 15:31:37 -0600

    First commit

---
+++ life/life.go
@@ -0,0 +1,6 @@
+package life
+
+// Meaning returns the meaning of life
+func Meaning() int {
+	return 42
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/swinton/golang-playground/life"
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Hello())
+	fmt.Println(life.Meaning())
+}
